6164b71edff530131153f892021a80c51556bdcc
stas maslovski <msv262626@gmail.com>
2021-08-23 14:50:26 +0300

    split asserts in MainPage

--- Google_Search/pages/main_page.py
+++ Google_Search/pages/main_page.py
@@ -5,10 +5,14 @@ from Google_Search import data
 
 class MainPage(BasePage):
 
-    def checking_up_the_searcher(self):
+    def search(self):
         self.find_elem(locators.FORM).send_keys(data.searching_phrase)
         self.press_enter(locators.FORM)
+
+    def should_be_more_than_10_results(self):
         assert self.get_result_number(locators.RESULT) > 10, \
-            'number of results should be more than 10'
+               'number of results should be more than 10'
+
+    def should_be_link_in_results(self):
         assert self.verify_availability_of_link(locators.LINKS, data.link), \
             'result should contain mvideo.ru '
--- Google_Search/tests.py
+++ Google_Search/tests.py
@@ -5,4 +5,7 @@ import data
 def test_for_google_search(browser):
     main_pg = MainPage(browser, data.main_url)
     main_pg.go_to_site()
-    main_pg.checking_up_the_searcher()
+    main_pg.search()
+    main_pg.should_be_more_than_10_results()
+    main_pg.should_be_link_in_results()
+
